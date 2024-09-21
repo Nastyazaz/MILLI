6b866a43d204960689ed8f5696e20f1f72b82259
Nastyazaz <dynaeva.nastya96@gmail.com>
2024-09-21 21:36:07 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@
 class main {
     public static void main(String[] args) {
         // Переменные для хранения стоимости билета и количества бонусных миль
-        int costOfTicket = 3500;
-        double bonusMiles = 0.0;
+        int costOfTicket = 13676;
+        int bonusMiles = 0;
 
 
         // Расчет количества бонусных миль
-        bonusMiles = costOfTicket * (1.0 / 20.0);
+        bonusMiles = (int) (costOfTicket * (1.0 / 20.0));
 
         // Округление до целого числа
         long roundedBonusMiles = (long) Math.ceil(bonusMiles);
