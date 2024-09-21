a510a1b544c6efe24a86eb435893a7f22813a55b
Nastyazaz <dynaeva.nastya96@gmail.com>
2024-09-21 12:13:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
 
-class BonusMilesCalculator {
+
+class main {
     public static void main(String[] args) {
         // Переменные для хранения стоимости билета и количества бонусных миль
-        double costOfTicket = 0.0;
+        int costOfTicket = 0.0;
         double bonusMiles = 0.0;
 
-        // Чтение стоимости билета с консоли
-        System.out.print("Введите стоимость вашего билета в рублях:");
-        try (Scanner scanner = new Scanner(System.in)) {
-            costOfTicket = scanner.nextDouble();
-        } catch (Exception e) {
-            System.err.println("Error reading input: " + e.getMessage());
-            return;
-        }
 
         // Расчет количества бонусных миль
         bonusMiles = costOfTicket * (1.0 / 20.0);
